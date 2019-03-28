e27e848df02b759971978d5ee018f4f21698e542
Tejas Kumar <tejas@tejas.qa>
2019-03-28 18:06:49 +0100

    Add useCallback wrapper
    Since `getQuote` is defined within the scope of the component, if I understand correctly, it will be recomputed on every render. We don't want this for performance, so I wrapped it in `useCallback`, passing `[]` as the second argument in order to tell React to not recompute the function on every render.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
@@ -6,11 +6,11 @@ import "./styles.css";
 function App() {
   const [quote, setQuote] = useState("");
 
-  const getQuote = async () => {
+  const getQuote = useCallback(async () => {
     const get = await fetch("https://api.kanye.rest");
     const json = await get.json();
     setQuote(json.quote);
-  };
+  }, []);
 
   useEffect(() => {
     getQuote();
